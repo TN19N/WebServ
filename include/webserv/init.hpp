52413db1a5223503990a2dfb391f810ff9c7f21c
Jaouad Chakir <jchakir@e2r2p6.1337.ma>
2023-03-07 13:52:32 +0100

    clean

--- include/webserv/init.hpp
+++
@@ -1,8 +0,0 @@
-# ifndef INIT_HPP
-# define INIT_HPP
-
-# include "webserv/context.hpp"
-
-size_t init(const Context* configuration, std::vector<pollfd>& fds);
-
-# endif
--- src/http/cgiHandler.cpp
+++
@@ -1,8 +0,0 @@
-# include "webserv/client.hpp"
-# include "webserv/context.hpp"
-# include "webserv/http.hpp"
-
-Client* HTTP::runCgi(const Context* location, const Client* client) {
-    int     pipeFd[2];
-    pid_t   pid;
-}
