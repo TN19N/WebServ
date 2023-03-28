bc2150dc0710ec1bef29d40e0e0ca0de663f3e1d
Mustapha Annouaoui <mannouao@e1r1p5.1337.ma>
2023-03-28 17:38:31 +0000

    fixe some bugs

--- www/entryPoint.py
+++ www/entryPoint.py
@@ -6,7 +6,7 @@ form = cgi.FieldStorage()
 cookies = environ["HTTP_COOKIE"].split(";")
 for cookie in cookies:
     if cookie.find("code=123") != -1:
-        print ("Location: /app", end="\r\n")
+        print ("Location: /app/", end="\r\n")
         print (end="\r\n")
         exit()
 
@@ -22,7 +22,7 @@ print("""
     </head>
     <body>
         <h1>First Time</h1>
-        <form action="/app" method="GET">
+        <form action="/app/" method="GET">
             <input type="submit" value="Go to /app">
         </form>
     </body>
