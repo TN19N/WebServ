ae7320aa8c6f1f03de29cbad1b157a8542dc61dd
Mustapha Annouaoui <mannouao@e1r1p5.1337.ma>
2023-03-30 14:29:36 +0000

    last commit

--- www/login/index.php
+++ www/login/index.php
@@ -11,7 +11,7 @@
 <html lang = "en">
 
    <head>
-      <title>Tutorialspoint.com</title>
+      <title>login.com</title>
       <link href = "css/bootstrap.min.css" rel = "stylesheet">
 
       <style>
@@ -77,18 +77,17 @@
 
       <h2>Enter Username and Password</h2>
       <div class = "container form-signin">
-
          <?php
             $msg = '';
 
             if (isset($_POST['login']) && !empty($_POST['username'])
                && !empty($_POST['password'])) {
 
-               if ($_POST['username'] == 'tutorialspoint' &&
-                  $_POST['password'] == '1234') {
+               if ($_POST['username'] == 'admin' &&
+                  $_POST['password'] == 'admin') {
                   $_SESSION['valid'] = true;
                   $_SESSION['timeout'] = time();
-                  $_SESSION['username'] = 'tutorialspoint';
+                  $_SESSION['username'] = 'admin';
 
                   echo 'You have entered valid use name and password';
                }else {
@@ -105,17 +104,15 @@
             ?>" method = "post">
             <h4 class = "form-signin-heading"><?php echo $msg; ?></h4>
             <input type = "text" class = "form-control"
-               name = "username" placeholder = "username = tutorialspoint"
+               name = "username" placeholder = "username"
                required autofocus></br>
             <input type = "password" class = "form-control"
-               name = "password" placeholder = "password = 1234" required>
+               name = "password" placeholder = "password" required>
             <button class = "btn btn-lg btn-primary btn-block" type = "submit"
                name = "login">Login</button>
          </form>
 
          Click here to clean <a href = "logout.php" tite = "Logout">Session.
-
       </div>
-
    </body>
 </html>
