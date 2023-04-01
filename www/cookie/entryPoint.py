c40d1ffd23d9d0df5568f3e6c1ca7b9ea5232301
Jaouad Chakir <jchakir@e2r2p6.1337.ma>
2023-04-01 21:10:42 +0000

    final

--- www/cookie/entryPoint.py
+++ www/cookie/entryPoint.py
@@ -4,3 +4,13 @@ from cookieGenerator import generate_cookie
 
 cookies = environ["HTTP_COOKIE"].split(";")
 method = environ["REQUEST_METHOD"]
+new_cookie = generate_cookie()
+
+print("Set-Cookie: session=", new_cookie,sep="", end="\r\n")
+
+print("\r\n")
+
+print("Your Old Cookie: ", end="")
+[print(cookie) for cookie in cookies if cookie[0:8] == "session="]
+
+print("\nYour New Cookie: session=", new_cookie, sep="")
